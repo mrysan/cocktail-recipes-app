bbd9aaf6aa7fd4d026b67ff54deb1a44c258401c
mrysan <mrysann@gmail.com>
2025-10-19 20:01:51 -0700

    feat: display favorite cocktails on favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,23 @@
 import CocktailModal from "../shared/CocktailModal";
+import { useState, useEffect } from "react";
+import { getFavorites } from "../storage/favoriteCocktails";
+import CocktailContainer from "../shared/CocktailContainer";
 
 export default function Favorites() {
+  const [favoriteCocktails, setFavoriteCocktails] = useState([]);
+
+  useEffect(() => {
+    const favorites = getFavorites();
+
+    setFavoriteCocktails(Object.values(favorites));
+  }, []);
+
+  console.log();
   return (
-    <div>
-      <h1>Favorites Page</h1>
-    </div>
+    <>
+      <div>
+        <CocktailContainer cocktailList={favoriteCocktails} />
+      </div>
+    </>
   );
 }
