aa89302d45fb155e9c4ac4ff93b5a26931fa8a88
mrysan <mrysann@gmail.com>
2025-10-19 17:57:48 -0700

    feat: favorite/un-favorite cocktails to local stoarge via modal

---
+++ src/storage/favoriteCocktails.js
@@ -0,0 +1,30 @@
+let favorites = {};
+
+function addFavorite(cocktail) {
+  let favorites = JSON.parse(localStorage.getItem("favorites"));
+  favorites[cocktail.idDrink] = cocktail;
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+function removeFavorite(cocktail) {
+  let favorites = JSON.parse(localStorage.getItem("favorites"));
+  delete favorites[cocktail.idDrink];
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+// returns true if cocktail is saved as a favorite, else false
+function isFavorite(cocktail) {
+  if (cocktail) {
+    const favorites = JSON.parse(localStorage.getItem("favorites"));
+    return favorites[cocktail.idDrink];
+  }
+  return false;
+}
+
+// returns an object of all the favorite cocktails
+function getFavorites() {
+  console.log(JSON.parse(localStorage.getItem("favorites")));
+  return JSON.parse(localStorage.getItem("favorites"));
+}
+
+export { addFavorite, removeFavorite, isFavorite, getFavorites };
