4423aa94291305b9a8d7c9214e18ce8b1b3c664a
mrysan <mrysann@gmail.com>
2025-10-21 15:01:11 -0700

    feat: Create content for About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,35 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
+      <h3>🥃 About This App</h3>
+      <p>
+        This app is your personal mixologist!
+        <ul>
+          <li> Search for cocktails by name</li>
+          <li>Browse cocktails alphabetically</li>
+          <li>Favorite cocktails to view later</li>
+        </ul>
+      </p>
+      <h3>🍸 Why I Built This</h3>
+      <p>
+        This app was built as a final project for the React Bootcamp by Code The
+        Dream.
+      </p>
+      <p>
+        It is a tribute to my time spent (and still spending) bartending, and is
+        a fusion of my passions: building software and crafting cocktails.
+      </p>
+      <h3>🙏 Acknowledgments</h3>
+
+      <p>
+        <a href="https://www.thecocktaildb.com/">TheCocktailDB </a>
+        for their free API that is responsible for all the cocktails you see!
+      </p>
+      <p>
+        <a href="https://codethedream.org"> Code The Dream</a> and all the folks
+        that make it happen.
+      </p>
+      <p>Chris Lee, my mentor.</p>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ function Home() {
     setApiError("");
   }
 
-  // initialize API once
+  // initialize API
   useEffect(() => {
     try {
       cocktailDbApi.current = new CocktailDbApi({
